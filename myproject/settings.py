29f8c001d11a14186869bda0a37fb9b7173d697a
jiril Zala <zjiril@gmail.com>
2020-07-27 01:21:36 +0530

    debut_false

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#3_06-tq(@#*betdm%bq-4a)5faoxnv_hk%3$2h!dl2tij7!hh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'donutscroll.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'donutscroll.herokuapp.com', '*']
 
 
 # Application definition
